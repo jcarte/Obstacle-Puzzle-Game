a26d78467f04efebedb136e5fbc10f63370fd21a
Takamon <nicolabirchall@gmail.com>
2017-02-22 21:02:21 +0000

    Canvas changes

--- Assets/Scripts/InputListener.cs
+++ Assets/Scripts/InputListener.cs
@@ -71,43 +71,49 @@ public class InputListener : MonoBehaviour
         //Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
 #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 			
-			//Check if Input has registered more than zero touches
-			if (Input.touchCount > 0)
+		//Check if Input has registered more than zero touches
+		if (Input.touchCount > 0)
+		{
+
+            //TODO JC bodge
+            Vector2 touchOrigin = Input.touches[0].position;
+
+
+
+            //Store the first touch detected.
+            Touch myTouch = Input.touches[0];
+				
+			//Check if the phase of that touch equals Began
+			if (myTouch.phase == TouchPhase.Began)
 			{
-				//Store the first touch detected.
-				Touch myTouch = Input.touches[0];
-				
-				//Check if the phase of that touch equals Began
-				if (myTouch.phase == TouchPhase.Began)
-				{
-					//If so, set touchOrigin to the position of that touch
-					touchOrigin = myTouch.position;
-				}
-				
-				//If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
-				else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
-				{
-					//Set touchEnd to equal the position of this touch
-					Vector2 touchEnd = myTouch.position;
-					
-					//Calculate the difference between the beginning and end of the touch on the x axis.
-					float x = touchEnd.x - touchOrigin.x;
-					
-					//Calculate the difference between the beginning and end of the touch on the y axis.
-					float y = touchEnd.y - touchOrigin.y;
-					
-					//Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
-					touchOrigin.x = -1;
-					
-					//Check if the difference along the x axis is greater than the difference along the y axis.
-					if (Mathf.Abs(x) > Mathf.Abs(y))
-						//If x is greater than zero, set horizontal to 1, otherwise set it to -1
-						horizontal = x > 0 ? 1 : -1;
-					else
-						//If y is greater than zero, set horizontal to 1, otherwise set it to -1
-						vertical = y > 0 ? 1 : -1;
-				}
+				//If so, set touchOrigin to the position of that touch
+				touchOrigin = myTouch.position;
 			}
+				
+			//If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+			else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+			{
+				//Set touchEnd to equal the position of this touch
+				Vector2 touchEnd = myTouch.position;
+					
+				//Calculate the difference between the beginning and end of the touch on the x axis.
+				float x = touchEnd.x - touchOrigin.x;
+					
+				//Calculate the difference between the beginning and end of the touch on the y axis.
+				float y = touchEnd.y - touchOrigin.y;
+					
+				//Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+				touchOrigin.x = -1;
+					
+				//Check if the difference along the x axis is greater than the difference along the y axis.
+				if (Mathf.Abs(x) > Mathf.Abs(y))
+					//If x is greater than zero, set horizontal to 1, otherwise set it to -1
+					horizontal = x > 0 ? 1 : -1;
+				else
+					//If y is greater than zero, set horizontal to 1, otherwise set it to -1
+					vertical = y > 0 ? 1 : -1;
+			}
+		}
 			
 #endif 
         //End of mobile platform dependendent compilation section started above with #elif
