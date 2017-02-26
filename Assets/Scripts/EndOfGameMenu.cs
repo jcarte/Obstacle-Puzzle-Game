a360d3ac9200cd9911a04e4ed28feba21ccefd7c
James Carter <jcarter1987@gmail.com>
2017-02-26 17:24:46 +0000

    Fixed level loading from main menu

--- Assets/Scripts/EndOfGameMenu.cs
+++ Assets/Scripts/EndOfGameMenu.cs
@@ -7,15 +7,28 @@ public class EndOfGameMenu : MonoBehaviour {
     Text movesTxt;
     Text resultTxt;
 
-    GameObject nextLevelButton;
-    
+    Button nextLevelButton;
+    Button mainMenuButton;
+    Button tryAgainButton;
 
-	// Use this for initialization
-	public void Init () 
+    // Use this for initialization
+    public void Init () 
     {
         movesTxt = transform.FindChild("ResultMoves").GetComponent<Text>();
         resultTxt = transform.FindChild("ResultCup").GetComponent<Text>();
-        nextLevelButton = transform.FindChild("EndOfGameMenuBarPanel").transform.FindChild("NextLevelButton").gameObject;
+
+        tryAgainButton = transform.FindChild("TryAgainButton").GetComponent<Button>();
+
+        GameObject panel = transform.FindChild("EndOfGameMenuBarPanel").gameObject;
+
+        nextLevelButton = panel.transform.FindChild("NextLevelButton").GetComponent<Button>();
+        mainMenuButton = panel.transform.FindChild("MainMenuButton").GetComponent<Button>();
+
+
+        nextLevelButton.onClick.AddListener(delegate () { GameManager.Instance.StartNextLevel(); });
+        mainMenuButton.onClick.AddListener(delegate () { GameManager.Instance.LoadMainMenu(); });
+        tryAgainButton.onClick.AddListener(delegate () { GameManager.Instance.RetryLevel(); });
+
     }
 	
 
@@ -35,7 +48,7 @@ public class EndOfGameMenu : MonoBehaviour {
 
         gameObject.SetActive(true);
 
-        nextLevelButton.SetActive(GameManager.Instance.HasNextLevel() && result != BoardManager.GameResult.Loss);
+        nextLevelButton.gameObject.SetActive(GameManager.Instance.HasNextLevel() && result != BoardManager.GameResult.Loss);
     }
 
     
